d1c349cf75289e7c53a9312810897e626756d413
Marika Allely and Mary Warrington <marywarrington@gmail.com>
2016-03-22 14:20:32 -0700

    add checkboxes

--- app/done.pipe.ts
+++ app/done.pipe.ts
@@ -2,17 +2,19 @@ import {Pipe, PipeTransform} from 'angular2/core';
 import {Task} from './task.model';
 
 @Pipe({
-
+  name: "done",
+  pure: false
 })
+
 export class DonePipe implements PipeTransform {
   transform(input: Task[], args) {
     var desiredDoneState = args[0];
     if(desiredDoneState === "done") {
-      return input.filter(function(task) {
+      return input.filter((task) => {
         return task.done;
       });
     } else if (desiredDoneState === "notDone") {
-      return input.filter(function(task) {
+      return input.filter((task) => {
         return !task.done;
       });
     }
