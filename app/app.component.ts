000a116e1d5c5e44d3d38093e95d1b7c1e1da998
Marika Allely and Mary Warrington <marywarrington@gmail.com>
2016-03-22 11:54:35 -0700

    add to-do list stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
-  <div class="container">
-    <h1>Skeleton Angular2 App!</h1>
-  <div>
+    <div class="container">
+      <h1>To-Do List</h1>
+      <task-list
+      [taskList]="tasks"
+      (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
   `
 })
 
 export class AppComponent {
-
+  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+  taskWasSelected(clickedTask: Task): void {
+    console.log("parent");
+  }
 }
